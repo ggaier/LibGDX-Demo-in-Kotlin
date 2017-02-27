ffee8041bc7c736e3aa180b9ac55a0a02538093e
walkingblind <jwenbo52@gmail.com>
2017-02-27 23:53:08 +0800

    add: AndroidLauncher

---
+++ app/src/main/java/com/ggaier/libgdxkotlin/AndroidLauncher.kt
@@ -0,0 +1,21 @@
+package com.ggaier.libgdxkotlin
+
+import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import main.java.com.ggaier.StartFiled
+
+/**
+ * Created by ggaier at 27/02/2017 .
+ * jwenbo52@Gmail.com
+ */
+class AndroidLauncher : AndroidApplication() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val androidApplicationConfig=AndroidApplicationConfiguration()
+        initialize(StartFiled(),androidApplicationConfig)
+    }
+
+
+}
--- core/src/main/java/com/ggaier/StartFiled.kt
+++ core/src/main/java/com/ggaier/StartFiled.kt
@@ -26,7 +26,7 @@ class StartFiled(): ApplicationAdapter(){
     override fun render() {
         Gdx.gl.glClearColor(0f,0f,0f,1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        
+
     }
 
 
