c69fe6f03a6e36063566961b49ceace948805564
walkingblind <jwenbo52@gmail.com>
2017-03-02 00:20:45 +0800

    add: draw concentric rectangles

--- app/src/com/ggaier/libgdxkotlin/AndroidLauncher.kt
+++ app/src/com/ggaier/libgdxkotlin/AndroidLauncher.kt
@@ -3,7 +3,7 @@ package com.ggaier.libgdxkotlin
 import android.os.Bundle
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
-import com.ggaier.gamedev.dots.ConnectTheDots
+import com.ggaier.gamedev.spirals.DrawASpiral
 
 /**
  * Created by ggaier at 27/02/2017 .
@@ -14,7 +14,8 @@ class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val androidApplicationConfig= AndroidApplicationConfiguration()
-        initialize(ConnectTheDots(),androidApplicationConfig)
+//        initialize(ConnectTheDots(),androidApplicationConfig)
+        initialize(DrawASpiral(),androidApplicationConfig)
     }
 
 
