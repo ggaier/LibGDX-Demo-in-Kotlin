c77d419717cf80e66cfaeb497e0d2cf7d83a6328
ggaier <jwenbo52@gmail.com>
2017-03-14 00:24:01 +0800

    add sierpinskitriangle class with nothing implemented

---
+++ core/src/com/ggaier/gamedev/sierpinskitriangle/SierpinskiTriangle.kt
@@ -0,0 +1,24 @@
+package com.ggaier.gamedev.sierpinskitriangle
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/**
+ * Created by ggaier at 14/03/2017 .
+ * jwenbo52@gmail.com
+ */
+class SierpinskiTriangle : ApplicationAdapter() {
+
+    override fun create() {
+    }
+
+
+    override fun render() {
+    }
+
+    override fun dispose() {
+    }
+
+    override fun resize(width: Int, height: Int) {
+    }
+
+}
