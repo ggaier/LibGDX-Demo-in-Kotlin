cb94df1c6c4664f93e762ad965f17c5e8c78ab8f
ggaier <jwenbo52@gmail.com>
2017-03-25 23:17:10 +0800

    add stub class

---
+++ core/src/com/ggaier/game/libgdxdemo/icicles/Constants.kt
@@ -0,0 +1,6 @@
+package com.ggaier.game.libgdxdemo.icicles
+
+/**
+ * Created by ggaier at 25/03/2017 .
+ * jwenbo52@gmail.com
+ */
---
+++ core/src/com/ggaier/game/libgdxdemo/icicles/Icicle.kt
@@ -0,0 +1,14 @@
+package com.ggaier.game.libgdxdemo.icicles
+
+/**
+ * Created by ggaier at 25/03/2017 .
+ * jwenbo52@gmail.com
+ */
+// ::class 获取到Kotlin class的引用，然后使用
+//Kotlin class的java property 获取到java 的类引用。
+private val TAG = Icicle::class.java.name
+
+class Icicle {
+    
+
+}
