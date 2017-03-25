0874b7dc5ea80a712385d9200c106e66c68f313e
ggaier <jwenbo52@gmail.com>
2017-03-25 23:49:13 +0800

    render my first icicle

--- core/src/com/ggaier/game/libgdxdemo/icicles/IcicleScreen.kt
+++ core/src/com/ggaier/game/libgdxdemo/icicles/IcicleScreen.kt
@@ -1,6 +1,8 @@
 package com.ggaier.game.libgdxdemo.icicles
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.viewport.ExtendViewport
@@ -16,20 +18,30 @@ class IcicleScreen : ScreenAdapter() {
     lateinit var mIcicle: Icicle
 
     override fun show() {
-        mIcicleViewport= ExtendViewport(WORLD_SIZE, WORLD_SIZE)
-        mRenderer= ShapeRenderer()
+        mIcicleViewport = ExtendViewport(WORLD_SIZE, WORLD_SIZE)
+        mRenderer = ShapeRenderer()
         mRenderer.setAutoShapeType(true)
-        mIcicle=Icicle(Vector2(WORLD_SIZE/2, WORLD_SIZE/2))
+        mIcicle = Icicle(Vector2(WORLD_SIZE / 2, WORLD_SIZE / 2))
+    }
+
+    override fun render(delta: Float) {
+        mIcicleViewport.apply()
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        mRenderer.projectionMatrix = mIcicleViewport.camera.combined
+        mRenderer.begin(ShapeRenderer.ShapeType.Filled)
+        mIcicle.render(mRenderer)
+        mRenderer.end()
     }
 
     override fun resize(width: Int, height: Int) {
-        mIcicleViewport.update(width,height)
+        mIcicleViewport.update(width, height)
     }
 
     override fun dispose() {
         mRenderer.dispose()
     }
-
 
 
 }
