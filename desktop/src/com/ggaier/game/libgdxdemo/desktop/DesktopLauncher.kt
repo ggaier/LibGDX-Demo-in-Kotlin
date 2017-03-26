dd01ca09eeb8fd81d41af274e3572f42b5b594c0
ggaier <jwenbo52@gmail.com>
2017-03-26 22:56:14 +0800

    update desktop game

--- desktop/src/com/ggaier/game/libgdxdemo/desktop/DesktopLauncher.kt
+++ desktop/src/com/ggaier/game/libgdxdemo/desktop/DesktopLauncher.kt
@@ -2,13 +2,13 @@ package com.ggaier.game.libgdxdemo.desktop
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
-import com.ggaier.game.libgdxdemo.userinput.inputtestbed.InputTestBed
+import com.ggaier.game.libgdxdemo.icicles.IciclesGame
 
 object DesktopLauncher {
 
     @JvmStatic fun main(arg: Array<String>) {
         val config = LwjglApplicationConfiguration()
-        LwjglApplication(InputTestBed(), config)
+        LwjglApplication(IciclesGame(), config)
     }
 
 }
